fcc97eba707cb1f8007d4443cd8d267e059c6b7d
Skuddin <sheikh@nextgenitltd.com>
2023-11-07 18:03:51 +0600

    splade install in laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<x-layout>
+    <x-slot name="header">
+        {{ __('Home') }}
+    </x-slot>
+
+    <x-panel class="flex flex-col items-center pt-16 pb-16">
+        <x-application-logo class="block h-12 w-auto" />
+
+        <div class="mt-8 text-2xl">
+            Welcome to your Splade application!
+
+        </div>
+        @php
+            $renderedMarkdown = 'Hlw this splade content';
+        @endphp
+        <!-- splade content component -->
+        <x-splade-content  as="article" class="text-2xl" :html="$renderedMarkdown" />
+
+        <!--splade data component-->
+        {{-- <x-splade-data :default="$user">
+            <input v-model="data.name" placeholder="Enter your name" />
+            <p>Your name is <span v-text="data.name" /></p>
+        </x-splade-data> --}}
+
+        <x-splade-data store="navigation" default="{ opened: false }" />
+
+        <!-- other elements... -->
+        <x-splade-defer url="http://127.0.0.1:8000/api/user">
+            <p v-show="processing">Loading data...</p>
+            <p v-for="data in response" v-text="data.name"/>
+            <button @click.prevent="reload">Reload</button>
+        </x-splade-defer>
+
+    </x-panel>
+</x-layout>
